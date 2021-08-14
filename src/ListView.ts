d40dcabe38c8e4c0653977af2143f15069389552
geoffreysflaminglasersword <garinstrader@gmail.com@users.noreply.github.com>
2021-08-13 22:19:47 -0500

    .

---
+++ src/ListView.ts
@@ -0,0 +1,104 @@
+import { ButtonComponent, ItemView, Menu, WorkspaceLeaf } from 'obsidian';
+import { CoalescerState, File, Item, Link, Tag } from "./SettingsAndUtils";
+
+import Coalescer from "./main";
+import List from './List.svelte'
+import { debug } from "svelte/internal";
+
+export const CoalesceListView = 'coalescer-view';
+
+export default class CoalescerListView extends ItemView {
+    private readonly plugin: Coalescer;
+    private data: CoalescerState;
+    comp:List;
+    items:Item[] = [];
+    button :ButtonComponent;
+
+    constructor(
+        leaf: WorkspaceLeaf,
+        plugin: Coalescer,
+        data: CoalescerState,
+    ) {
+        super(leaf);
+
+        this.plugin = plugin;
+        this.data = data;
+        this.app.workspace.onLayoutReady(() => this.redraw())
+    }
+
+    public getViewType(): string { return CoalesceListView; }
+    public getDisplayText(): string { return 'Coalescer'; }
+    public getIcon(): string { return 'clock'; }
+
+    public onHeaderMenu(menu: Menu): void {
+        menu
+            .addItem((item) => {
+                item
+                    .setTitle('Refresh')
+                    .setIcon('sweep')
+                    .onClick(async () => await this.plugin.saveData(true));
+            })
+            .addItem((item) => {
+                item
+                    .setTitle('Close')
+                    .setIcon('cross')
+                    .onClick(() => {
+                        this.app.workspace.detachLeavesOfType(CoalesceListView);
+                    });
+            });
+    }
+
+    
+    public load(): void {
+        super.load();
+    }
+
+    public readonly redraw = (): void => {
+        console.log(`this.plugin.state`, this.plugin.state)
+        this.comp?.$destroy();
+        this.items = []
+        for (const [key, value] of Object.entries((this.app.metadataCache as any).getTags())) {
+            if(this.plugin.state.omittedTags.some(matcher => key.match(new RegExp(matcher,'i')))) continue;
+            this.items.push(new Tag(key.replace(/#/,''),value))
+        }
+
+        const matchFileName = /(?<=[\/\\]|^)([^\/\\]+?)(?:\.md)?$/; //also matches when it's just a name, e.g. 'file.md', with no slashes
+
+        
+        let links = new Map<string,number>();
+        
+        let extractLinks=(cachedLinks:any[])  => {
+            cachedLinks.forEach(([path, link]) => {
+                if (this.plugin.state.omittedPaths.some(matcher => path.match(new RegExp(matcher, 'i'))))
+                    return;
+                Object.entries(link).forEach(([name, count]) => {
+                    name = name.match(matchFileName, '$1')[1];
+                    links.set(name, links.get(name) ? links.get(name) + count : count);
+                });
+            });
+        }
+        extractLinks(Object.entries(this.app.metadataCache.resolvedLinks));
+        links.forEach((value,key) =>  this.items.push(new Link(key,value,true)))
+        links.clear();
+        extractLinks(Object.entries(this.app.metadataCache.unresolvedLinks));
+        links.forEach((value,key) =>  this.items.push(new Link(key,value,false)))
+
+
+        this.app.vault.getMarkdownFiles().filter(
+            f => this.plugin.state.omittedPaths.every(p => !p || !f.path.match(new RegExp(p,'i')))
+            ).map(v => new File(v)).forEach(v => this.items.push(v));
+
+        const contentEl = this.containerEl.children[1];
+        this.comp = new List({
+            target: contentEl,
+            props: {
+                items: this.items,
+                app: this.app,
+                state:this.plugin.state,
+                plugin:this.plugin
+            }
+        });
+  
+    };
+
+}
---
+++ src/main.ts
@@ -0,0 +1,64 @@
+import * as O from 'obsidian'
+
+import CoalescerListView, { CoalesceListView } from "./ListView";
+import CoalescerSettingsTab, { CoalescerState, DEFAULT_SETTINGS } from "./SettingsAndUtils";
+import { Plugin, WorkspaceLeaf } from 'obsidian';
+
+export default class Coalescer extends Plugin {
+	state: CoalescerState;
+	view: CoalescerListView;
+
+	async onload() {
+		console.log('Loading Coalescer v' + this.manifest.version);
+		await this.loadData();
+		
+		this.addSettingTab(new CoalescerSettingsTab(this.app, this));
+		this.registerView(CoalesceListView,
+		  (leaf) => (this.view = new CoalescerListView(leaf, this, this.state)),
+		);
+	
+		this.app.workspace.onLayoutReady(this.initView);
+		this.registerEvent(this.app.vault.on('create', this.saveData.bind(this)));
+		this.registerEvent(this.app.vault.on('rename', this.saveData.bind(this)));
+		this.registerEvent(this.app.vault.on('delete', this.saveData.bind(this)));
+		(this.app.workspace as any).registerHoverLinkSource(
+			CoalesceListView,{display: 'Coalescer',defaultMod: true}
+		);
+		this.app.workspace.on('active-leaf-change',(leaf) => {
+
+		});
+		
+
+	}
+
+	onunload() {
+		console.log('unloading Coalescer');
+		(this.app.workspace as any).unregisterHoverLinkSource(CoalesceListView);
+	}
+
+	public async loadData() {let r = await super.loadData(); console.log(`r`, r); this.state = Object.assign({}, DEFAULT_SETTINGS, r);}
+	public async saveData(redraw:boolean) { 
+		console.log(`this.state`, this.state)
+		await super.saveData(this.state); 
+		if(redraw) this.view?.redraw();
+	}
+  
+	private readonly initView = async () => {
+	  let leaf: WorkspaceLeaf = null;
+	  for (leaf of this.app.workspace.getLeavesOfType(CoalesceListView)) {
+		if (leaf.view instanceof CoalescerListView) return;
+		await leaf.setViewState({ type: 'empty' });
+		break;
+	  }
+	  (leaf ?? this.app.workspace.getLeftLeaf(false)).setViewState({
+		type: CoalesceListView,
+		active: true,
+	  });
+	};
+	
+}
+
+
+
+  
+  
